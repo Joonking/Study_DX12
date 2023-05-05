5ec17b092f1e983bd363dbafebf91e748c9c1cd3
MinJoon Kim <kmj73203@naver.com>
2023-05-06 01:33:15 +0900

    002 장치 초기화

--- Engine/EnginePch.cpp
+++ Engine/EnginePch.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "EnginePch.h"
+#include "Engine.h"
 
-void HelloEngine()
-{
+//엔진 객체를 전역으로 EnginePch 에 선언해두고
+//다른곳에서 모두 사용하도록
+//유니크 포인터를 이용해서 안전하게
+unique_ptr<Engine> GEngine = make_unique<Engine>();
 
-}
