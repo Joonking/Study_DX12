779d21ae7a1b3baf34140406bd3b96ad8b195117
MinJoon Kim <kmj73203@naver.com>
2023-06-14 13:40:06 +0900

    01-3 삼각형 띄우기 복습

--- Engine/EnginePch.h
+++ Engine/EnginePch.h
@@ -62,8 +62,8 @@ struct Vertex
 	Vec4 color;
 };
 
-#define DEVICE			GEngine->GetDevice()->GetDevice()
-#define CMD_LIST		GEngine->GetCmdQueue()->GetCmdList()
-#define ROOT_SIGNATURE	GEngine->GetRootSignature()->GetSignature()
+#define DEVICE					GEngine->GetDevice()->GetDevice()
+#define CMD_LIST					GEngine->GetCmdQueue()->GetCmdList()
+#define ROOT_SIGNATURE		GEngine->GetRootSignature()->GetSignature()
 
 extern unique_ptr<class Engine> GEngine;
